92311eaed059a04a7853a6cc04120cba4ae8ce1a
FahViel <fahrimaulana007@gmail.com>
2023-02-01 22:08:00 +0700

    Update portoflio real estate app (fixing chakra UI with chakraProvider dan menambahkan detail property)

--- real-estate-app/my-app/pages/_app.js
+++ real-estate-app/my-app/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import Router from 'next/router'
+import Head from 'next/head'
+import NProgress from 'nprogress'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Layout from '../components/Layout'
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  )
 }
+
+export default App
